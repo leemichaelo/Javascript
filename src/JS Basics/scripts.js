22680bf46cf72ac50ab9362e3cba253b48740c98
Lee Owen <LeeMichaelO@outlook.com>
2018-06-09 07:28:24 -0700

    Story Game With Prompts and Strings

--- src/JS Basics/scripts.js
+++ src/JS Basics/scripts.js
@@ -1,5 +1,11 @@
-﻿var input = prompt("Enter an adjective.");
-var message = "The " + input + " kids went to the ";
-input = prompt("Enter a noun.");
-message += input + "." ;
-document.write(message);
+﻿//<h2>There once was a [adjective] slug who wanted to climb up a tree to [eat] the [noun].</h2>
+
+var adjective = prompt("Give me an adjective");
+var part1 = "There once was a " + adjective;
+var verb = prompt("Give me a verb");
+var part2 = "slug who wanted to climb up a tree to " + verb;
+var noun = prompt("Give me a noun");
+var part3 = "the " + noun;
+alert("You're finished!");
+var sentence = part1 + " " + part2 + " " + part3;
+document.write(sentence);
