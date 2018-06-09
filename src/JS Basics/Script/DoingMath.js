0d420b4d03535df1b1e9e717633de0a7ee4c3de7
Lee Owen <LeeMichaelO@outlook.com>
2018-06-09 07:55:17 -0700

    Added calculations to determine seconds alive by years alive

--- src/JS Basics/Script/DoingMath.js
+++ src/JS Basics/Script/DoingMath.js
@@ -1,1 +1,11 @@
-﻿
+﻿var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var myAge = 29;
+
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write("There are " + secondsPerDay + " seconds in a day");
+var secondsAlive = secondsPerDay * daysPerWeek * weeksPerYear * myAge;
+document.write("<br> I have been alive for more than " + secondsAlive + " seconds!");
