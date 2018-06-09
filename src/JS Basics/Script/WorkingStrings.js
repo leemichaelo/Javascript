d8b78ae7cb44a904767db2679bcd8014ec3afa7e
Lee Owen <LeeMichaelO@outlook.com>
2018-06-09 08:26:40 -0700

    Did some refactoring on WorkingStrings

--- src/JS Basics/Script/WorkingStrings.js
+++ src/JS Basics/Script/WorkingStrings.js
@@ -1,23 +1,22 @@
 ﻿//<h2>There once was a [adjective] slug who wanted to climb up a tree to [eat] the [noun].</h2>
 
 var numberOfQuestions = 3;
-var questionsLeft = "There are " + numberOfQuestions + " questions left!";
 
-var adjective = prompt(questionsLeft + "\nGive me an adjective");
-numberOfQuestions -= 1;
-var questionsLeft = "There are " + numberOfQuestions + " questions left!";
+var adjective = prompt(questionsLeft(1) + "\nGive me an adjective");
 
 var part1 = "There once was a " + adjective;
-var verb = prompt(questionsLeft + "\nGive me a verb");
-numberOfQuestions -= 1;
-var questionsLeft = "There are " + numberOfQuestions + " questions left!";
+var verb = prompt(questionsLeft(1) + "\nGive me a verb");
 
 var part2 = "slug who wanted to climb up a tree to " + verb;
-var noun = prompt(questionsLeft + "\nGive me a noun");
-numberOfQuestions -= 1;
-var questionsLeft = "There are " + numberOfQuestions + " questions left!";
+var noun = prompt(questionsLeft(1) + "\nGive me a noun");
 
 var part3 = "the " + noun;
-alert(questionsLeft + "\nYou're finished!");
+alert(questionsLeft(0) + "\nYou're finished!");
 var sentence = part1 + " " + part2 + " " + part3;
 document.write(sentence);
+
+function questionsLeft(reduceby) { 
+    var messageToReturn = "There are " + numberOfQuestions + " questions left!";
+    numberOfQuestions -= reduceby;
+    return messageToReturn;
+}
