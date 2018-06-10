5af8599fa83c3b3e79355ee1a50aa669597b6b0b
Lee Owen <LeeMichaelO@outlook.com>
2018-06-10 14:24:35 -0700

    Exploring comparisons

---
+++ src/JS Basics/Script/Comparisons.js
@@ -0,0 +1,8 @@
+﻿var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = prompt("I'm think of a number between 1 and 6. What is it?");
+if (parseInt(guess) === randomNumber) {
+    document.write("<p>You guessed the number!</p>");
+}
+else {
+    document.write("<p>Sorry. THe number was " + randomNumber + ".</p>");
+}
---
+++ src/JS Basics/Script/Quiz.js
@@ -0,0 +1,6 @@
+﻿var answer = prompt("What programming language is the name of a gem?");
+if (answer === "Ruby") {
+    document.write("That is correct!");
+}
+else
+    document.write("That is incorrect...");
