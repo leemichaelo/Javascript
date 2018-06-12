c9fe7a1162840e066e6cc1f07b9f3b74146c3ed3
Lee Owen <LeeMichaelO@outlook.com>
2018-06-11 20:01:06 -0700

    Added conditional loops to give the player more chances to guess and comments

--- src/JS Basics/Script/Comparisons.js
+++ src/JS Basics/Script/Comparisons.js
@@ -1,8 +1,34 @@
-﻿var randomNumber = Math.floor(Math.random() * 6) + 1;
+﻿/*The Random Number Guessing Game
+ * Generates a number between 1 and 6
+ * and gives the player two attempts to guess the number*/
+
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
 var guess = prompt("I'm think of a number between 1 and 6. What is it?");
+
+//Checks the user input to see if it is equal the random number
 if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+}
+//Checks the user input to see if it is less than the random number
+else if (parseInt(guess) < randomNumber) {
+    var guessAgain = prompt("Your choice of " + guess + ", was lower than the random number. Try again!");
+    if (parseInt(guess) === randomNumber) {
+        correctGuess = true;
+    }
+}
+//Checks the user input to see if it is greater than the random number
+else if (parseInt(guess) > randomNumber) {
+    var guessAgain = prompt("Your choice of " + guess + ", was greater than the random number.Try again!");
+    if (parseInt(guess) === randomNumber) {
+        correctGuess = true;
+    }
+}
+
+//Checks if the user had guessed the right number at the end or not
+if (correctGuess) {
     document.write("<p>You guessed the number!</p>");
 }
 else {
-    document.write("<p>Sorry. THe number was " + randomNumber + ".</p>");
+    document.write("<p>Sorry. The number was " + randomNumber + ".</p>");
 }
