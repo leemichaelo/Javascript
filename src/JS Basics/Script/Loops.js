1e9206150f67fb32884a97385e74b8cea2fdc0a6
Lee Owen <LeeMichaelO@outlook.com>
2018-06-12 06:03:38 -0700

    Exploring while loops

---
+++ src/JS Basics/Script/Loops.js
@@ -0,0 +1,20 @@
+﻿//Initialize Variables
+var guessThis = randomNumber(6);
+var guessAttempts = 0;
+var computerGuess = 0;
+
+//Define functions
+function randomNumber(upper) {
+    return Math.floor(Math.random() * upper) + 1;
+}
+
+//Runs the loop
+while (computerGuess !== guessThis) {
+    computerGuess = randomNumber(6);
+    guessAttempts += 1;
+}
+
+//Prints the results
+document.write("The random number was: " + guessThis);
+document.writeln("<br><br>It took the computer " + guessAttempts +
+    " guesses to get the correct answer.");
