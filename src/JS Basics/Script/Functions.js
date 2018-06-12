80bec8e270c3417dd13c7afb89190fd9393f30cd
Lee Owen <LeeMichaelO@outlook.com>
2018-06-11 22:18:06 -0700

    Working with functions and throwing exceptions

---
+++ src/JS Basics/Script/Functions.js
@@ -0,0 +1,20 @@
+﻿function getRandomNumber(upper, lower) {
+    if (isNaN(upper) || isNaN(lower)) {
+        throw new Error("You must use numbers only!");
+    }
+    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+function getArea(width, length, unit) {
+    var area = width * length;
+    return area + " " + unit;
+}
+
+document.write(getRandomNumber(9, 5));
+
+
+/*function expression
+var alertRandom = function () {
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    alert(randomNumber);
+};*/
