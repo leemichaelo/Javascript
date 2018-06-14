662da2cda9a1141fcbd7196a9afb16eaedd8f394
Lee Owen <LeeMichaelO@outlook.com>
2018-06-14 09:48:50 -0700

    Added a search feature to look through an array of inStock Items

--- src/JS Basics/Script/search.js
+++ src/JS Basics/Script/search.js
@@ -1,4 +1,4 @@
-﻿var shoppingList = ["apples",
+﻿var inStock = ["apples",
     "eggs",
     "milk",
     "cookies",
@@ -23,7 +23,17 @@ function print(message) {
 
 while (true) {
     search = prompt("Search for a product in our store. Type 'list' to show all of the produce and 'quit' to exit");
-    if (search === 'quit') {
-
+    if (search.toLowerCase() === 'quit') {
+        break;
+    }
+    else if (search.toLowerCase() === 'list') {
+        print("We currently have the following in stock <br>" +
+            inStock.join(", "));
+    }
+    else if (inStock.indexOf(search.toLowerCase()) !== -1) {
+        print("We do have " + search + " in stock!");
+    }
+    else {
+        print("We currently do not have " + search + " in stock...");
     }
 }
