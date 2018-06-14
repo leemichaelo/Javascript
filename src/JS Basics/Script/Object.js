1416801384855a8aa878ce8e6d3b8dd26b8f0289
Lee Owen <LeeMichaelO@outlook.com>
2018-06-14 14:16:03 -0700

    Working with Objects

---
+++ src/JS Basics/Script/Object.js
@@ -0,0 +1,20 @@
+﻿var person = {
+    name: "Lee",
+    country: "USA",
+    age: 29,
+    treehouseStudent: true,
+    skills: ["JavaScript", "HTML", "CSS"]
+};
+
+function print(message) {
+    var div = document.getElementById("output");
+    div.innerHTML = message;
+}
+
+var message = '<p>Hello. My name is ' + person.name + '</p>';
+message += '<p>I live in the ' + person.country + '</p>';
+person.name = 'Lee Owen';
+message += '<p>But, full; name is ' + person.name + '</p>';
+message += '<p>I have ' + person.skills.length + ' skills: ';
+message += person.skills.join(", ");
+print(message);
